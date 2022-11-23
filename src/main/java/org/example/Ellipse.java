7fe553892838ad34902e0703d8d82049a2dd01b0
VitaliyJV <114652130+VitaliyJV@users.noreply.github.com>
2022-11-23 12:31:11 +0200

    1
    Classes have been created

---
+++ src/main/java/org/example/Ellipse.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Ellipse extends Shape{
+    private double R1;
+    private double R2;
+    private double Exc;
+
+    public void setR1(double r1) {
+        R1 = r1;
+    }
+    public double getR1() {
+        return R1;
+    }
+    public void setR2(double r2) {
+        R2 = r2;
+    }
+    public double getR2() {
+        return R2;
+    }
+       public void setExc(double exc) {
+        Exc = exc;
+    }
+    public double getExc() {
+        return Exc;
+    }
+    @Override
+    public String outingOfShape() {
+        return "Ellipse";
+    }
+}
